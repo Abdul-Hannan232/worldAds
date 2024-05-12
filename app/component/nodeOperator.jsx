3435a8371d4c7d9f9ee64c9fe0842f623f2c69eb
Abdul-Hannan232 <abdulhani232@gmail.com>
2024-05-12 21:34:21 +0500

    add arrow

--- app/component/nodeOperator.jsx
+++ app/component/nodeOperator.jsx
@@ -25,6 +25,9 @@ const NodeOperator = () => {
                     <Image src="/Group 1.png" alt="Image" width={203} height={207} className="lg:w-[493px] lg:h-[497px] lg:mx-0 mx-auto " />
                 </div>
                 <div className="xl:w-[500px]  md:w-[400px] lg:mt-0 mt-10 lg:mx-0 mx-auto">
+                <img src="./arrow2.png" className=" hidden lg:block h-32 lg:h-auto w-[136px]  mb-6 xl:-ml-8  " />
+                <img src="./arrow3.png" className="  lg:hidden  m-auto  my-6  " />
+
                     <h1 className="xl:text-6xl md:text-5xl text-3xl font-medium lg:text-start text-center text-[#181818]">Node operators</h1>
                     <p className="xl:text-xl lg:text-sm text-md font-light lg:w-auto md:w-80  lg:text-sm md:text-md text-xs  lg:mx-0 mx-auto text-[#030303] lg:text-start text-center mt-5">Become a cornerstone of digital advertising. Own a smart digital screen and start earning as advertisers choose your prime ad slots.</p>
                     <div className="border border-gray-300 xl:mt-14 mt-10 rounded-full lg:mx-0 mx-auto  xl:w-44 w-36 ">
